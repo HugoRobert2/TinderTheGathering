daebd09a5ad4901048d8b2a72d01b095021acf55
Clement <clemillard@gmail.com>
2020-11-10 16:21:52 +0100

    navigation init

--- src/main/java/com/example/tinderthegathering/ui/profile/notifications/ProfileViewModel.java
+++ src/main/java/com/example/tinderthegathering/ui/profile/notifications/ProfileViewModel.java
@@ -1,16 +1,16 @@
-package com.example.tinderthegathering.ui.dashboard;
+package com.example.tinderthegathering.ui.profile.notifications;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class ProfileViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public ProfileViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        mText.setValue("This is home fragment");
     }
 
     public LiveData<String> getText() {
